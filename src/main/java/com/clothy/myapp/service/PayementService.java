2a558a87d5f37aec6bf8133bc34b2bdf0ba7c003
oueslati <yosri.oueslati33@gmail.com>
2025-09-16 09:39:15 +0200

    minor changes

--- src/main/java/com/clothy/myapp/service/PayementService.java
+++ src/main/java/com/clothy/myapp/service/PayementService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 public class PayementService {
 
     public Session createCheckoutSession(Long amount) throws Exception {
-        Stripe.apiKey = "clé stripe";
+        Stripe.apiKey = "fezfjoezf";
 
         SessionCreateParams params = SessionCreateParams.builder()
             .setMode(SessionCreateParams.Mode.PAYMENT)
