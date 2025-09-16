7753af696109487b5904f1a4983c0ede02e84b5e
NoixDePacan <pa.vedrenne@hotmail.fr>
2025-09-16 14:03:59 +0200

    entities dans un menu deroulant connexion depuis le footer

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { TranslateDirective } from 'app/shared/language';
 
 @Component({
   selector: 'jhi-footer',
   templateUrl: './footer.component.html',
-  imports: [TranslateDirective],
+  imports: [TranslateDirective, RouterLink],
 })
 export default class FooterComponent {}
