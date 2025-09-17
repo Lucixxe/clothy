2e5ec8421f647e9e06ca9ef2c92962bd2a048aab
nafis <nafisw27@gmail.com>
2025-09-17 10:13:15 +0200

    stripe csp updateeeee

--- src/main/webapp/environments/environment.development.ts
+++ src/main/webapp/environments/environment.development.ts
@@ -3,5 +3,7 @@ export const environment = {
   DEBUG_INFO_ENABLED: true,
   production: false,
   apiUrl: 'http://localhost:8080',
-  stripePublicKey: 'pk_test_51S7sx2RkxlAub16WafblID2ttD1zpzIgRSLAbYsRENxi1UueZZtPDMrZK8sR8Im0hvhzYhuqCKVFRFZAQDC9B3tL00Zd6tYxnP',
+  stripePublicKey: 'pk_test_...',
 };
+
+console.log('ENV DEV:', environment);
