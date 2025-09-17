eff8de1043a81a9c4984ffa6ca4285b1f777a839
nafis <nafisw27@gmail.com>
2025-09-17 09:44:10 +0200

    stripe csp update

--- src/main/webapp/environments/environment.development.ts
+++ src/main/webapp/environments/environment.development.ts
@@ -1,4 +1,7 @@
 export const environment = {
-  VERSION: 'DEV',
+  VERSION: __VERSION__,
   DEBUG_INFO_ENABLED: true,
+  production: false,
+  apiUrl: 'http://localhost:8080',
+  stripePublicKey: 'pk_test_51S7sx2RkxlAub16WafblID2ttD1zpzIgRSLAbYsRENxi1UueZZtPDMrZK8sR8Im0hvhzYhuqCKVFRFZAQDC9B3tL00Zd6tYxnP',
 };
--- src/main/webapp/environments/environment.ts
+++ src/main/webapp/environments/environment.ts
@@ -1,4 +1,7 @@
 export const environment = {
   VERSION: __VERSION__,
   DEBUG_INFO_ENABLED: false,
+  production: true,
+  apiUrl: 'https://clothy-a3b2d8314e46.herokuapp.com',
+  stripePublicKey: 'pk_test_51S7sx2RkxlAub16WafblID2ttD1zpzIgRSLAbYsRENxi1UueZZtPDMrZK8sR8Im0hvhzYhuqCKVFRFZAQDC9B3tL00Zd6tYxnP',
 };
