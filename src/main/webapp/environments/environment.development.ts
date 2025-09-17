0835193b4d1288927cd3172c998d28d38b131ebf
nafis <nafisw27@gmail.com>
2025-09-17 11:30:26 +0200

    merge stripe deply

--- src/main/webapp/environments/environment.development.ts
+++ src/main/webapp/environments/environment.development.ts
@@ -3,8 +3,8 @@ export const environment = {
   DEBUG_INFO_ENABLED: true,
   production: false,
   apiUrl: 'http://localhost:8080',
-  stripePublicKey: '',
-  stripeSecretKey: '',
+  stripePublicKey: 'pk_test_51S7vEeFGOPXjlvLiazDi3wt7979MTWp9CnrKskAaAL9BFrMpUshe755kofZxdl7yqofpdP9gejTYrewCDcH8RDnP002bA39jxT',
+  stripeSecretKey: 'sk_test_51S7vEeFGOPXjlvLi0VuI8Y4FqXjQz6drLnaCxXaqpatAeMqzIf0zPtBdRJGPPDYdCVLh0JLuUqtp5p2XltU8P7ZI00MpOgxiy8',
 };
 
 console.log('ENV DEV:', environment);
